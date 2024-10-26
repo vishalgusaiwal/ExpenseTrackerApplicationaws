38a944554f19bb279d39d0763e879136cbccf7c5
vishalgusaiwal9352 <vishalgusaiwal66@gmail.com>
2024-10-26 11:08:56 +0530

    changes done for production deployments

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    env: process.env.NODE_ENV || 'development',
+    env: process.env.NODE_ENV || 'production',
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "secret",
     mongoUri: process.env.MONGODB_URI || "mongodb+srv://Expenseadmin:admin123@cluster0.f8jlm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
