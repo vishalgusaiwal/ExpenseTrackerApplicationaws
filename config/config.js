f4051082ef15fc97b2039622dc988de8ec774a48
vishalgusaiwal9352 <vishalgusaiwal66@gmail.com>
2024-10-26 09:06:06 +0530

    commit for application setup

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "secret",
-    mongoUri: process.env.MONGODB_URI || "URL"
+    mongoUri: process.env.MONGODB_URI || "mongodb+srv://Expenseadmin:admin123@cluster0.f8jlm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 }
 export default config;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 import config from './../config/config.js';
 import app from './express.js';
+import mongoose from 'mongoose';
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri);
+mongoose.connection.on('error', () => {
+    console.log('unable to connect to database : ', config.mongoUri);
+});
 
 app.listen(config.port, (err) => {
     if (err) {
